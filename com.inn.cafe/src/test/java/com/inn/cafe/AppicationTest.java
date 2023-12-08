f7bd445700e96bcb214a51e5406180683856db96
vthakur012 <63159301+vthakur012@users.noreply.github.com>
2023-12-08 15:22:03 +0530

    Initial Commit

---
+++ com.inn.cafe/src/test/java/com/inn/cafe/AppicationTest.java
@@ -0,0 +1,11 @@
+package com.inn.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
